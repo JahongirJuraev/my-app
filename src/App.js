376df71bb1c24f7ac55561c578e96b854182dd8c
JahongirJuraev <jahongirjuraev202@gmail.com>
2023-10-20 10:06:22 +0500

    kkk

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Person from './Person';
+import './App.css';
+import BookList from './Components/BookList';
+
+// function App() {
+//   let numb = 203;
+//   return (
+//     <div className="App">
+//         <h1>React Counter</h1>
+//         <Person name="Jack" age="32" />
+//         <Person name="Js" age="10" />
+//         <h1>My Favourite number is {numb}</h1>
+//         <Person />
+//         <Person />
+//     </div>
+//   );
+// };
+// export default App;
+
+class App extends React.Component{
+   render() {
+    return <h1>Привет</h1>
+  }
+}
+
+export default App;
+
+
